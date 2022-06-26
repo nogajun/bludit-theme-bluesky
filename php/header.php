a2f8c61fe38d73facc41062334a2e42ba382ee38
Jun Nogata <nogajun@gmail.com>
2022-06-26 12:46:35 +0900

    ページネーション作成

--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
   <!-- header -->
   <header class="header row m-0 p-0 shadow">
 
-    <nav class="navbar navbar-expand-lg sticky-top navbar-dark header__navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark header__navbar">
       <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title() ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,11 +30,11 @@
 
     <div class="header__static_page_menu d-flex align-items-end m-0 p-0 w-100">
       <nav class="container-lg my-0 p-0">
-        <ul class="d-flex list-unstyled my-0">
+        <ul class="d-flex flex-wrap list-unstyled my-0">
           <?php
           foreach ($categories->db as $key => $fields) :
             if ($fields['list']) :
-              echo '<li class="text-center w-100 p-2"><a href="' . DOMAIN_CATEGORIES . $key . '" class="text-white text-uppercase fw-bold">' . $fields['name'] . '</a></li>';
+              echo '<li class="text-center col-sm"><a href="' . DOMAIN_CATEGORIES . $key . '" class="d-inline-flex align-middle text-white text-uppercase p-3 fw-bold">' . $fields['name'] . '</a></li>';
             endif;
           endforeach;
           ?>
--- php/home.php
+++ php/home.php
@@ -1,6 +1,8 @@
 <div class="row row-cols-1 row-cols-md-2 gy-2 g-md-2 g-lg-4">
 
-  <?php if (empty($content)) { $language->p('No pages found'); } ?>
+  <?php if (empty($content)) {
+    $language->p('No pages found');
+  } ?>
   <?php foreach ($content as $page) : ?>
 
     <!-- card -->
@@ -13,9 +15,9 @@
         <section class="card-body">
           <h2 class="card-title h5 fw-bold"><?php echo $page->title(); ?></h2>
           <p class="card-text">
-          <?php echo (empty($page->description())?$language->p('Complete the description of the article'):$page->description()) ?>
+            <?php echo (empty($page->description()) ? $language->p('Complete the description of the article') : $page->description()) ?>
           </p>
-          <p class="card-text text-end"><?php echo $page->readMore()?$L->get('Read more'):'' ?></p>
+          <p class="card-text text-end"><?php echo $page->readMore() ? $L->get('Read more') : '' ?></p>
         </section>
         <footer class="card-footer bg-white text-end fw-bold border-0">
           <time datetime="2022-06-21" class="card-date me-1"><?php echo $page->date(); ?></time>
@@ -26,20 +28,18 @@
   <?php endforeach; ?>
 </div>
 
-<nav aria-label="Page navigation" class="m-lg-4">
-  <ul class="pagination justify-content-center">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
+<?php if (Paginator::numberOfPages() > 1) : ?>
+  <div class="container pt-2 pb-5 m-lg-4 text-center">
+    <div class="row">
+      <div class="col">
+        <?php if (Paginator::showPrev()) : ?>
+          <a href="<?php echo Paginator::previousPageUrl() ?>" class="btn btn-sm">&larr; Previous</a>
+        <?php endif ?>
+        <?php if (Paginator::showNext()) : ?>
+          <a href="<?php echo Paginator::nextPageUrl() ?>" class="btn btn-sm">Next &rarr;</a>
+        <?php endif ?>
+      </div>
+    </div>
+  </div>
+<?php endif ?>
+
