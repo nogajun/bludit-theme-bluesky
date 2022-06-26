fea2818b746fc4e0564f381d6e9e89244da18117
Jun Nogata <nogajun@gmail.com>
2022-06-26 09:59:21 +0900

    ヘッダーメニュー作成

--- php/header.php
+++ php/header.php
@@ -4,17 +4,22 @@
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark header__navbar">
       <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title() ?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-          <?php foreach ($staticContent as $staticPage): ?>
-            <li class="nav-item"><a class="nav-link" href="<?php echo $staticPage->permalink() ?>"><?php echo $staticPage->title() ?></a></li>
-  				<?php endforeach ?>
-          </ul>
-        </div>
+        <?php if ($staticContent) : ?>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <?php
+              foreach ($staticContent as $menu) :
+                if (!$menu->isChild()) :
+                  echo '<li class="nav-item"><a class="nav-link active" href="' . $menu->permalink() . '">' . $menu->title() . '</a></li>';
+                endif;
+              endforeach;
+              ?>
+            </ul>
+          </div>
+        <?php endif; ?>
       </div>
     </nav>
 
@@ -26,10 +31,13 @@
     <div class="header__static_page_menu d-flex align-items-end m-0 p-0 w-100">
       <nav class="container-lg my-0 p-0">
         <ul class="d-flex list-unstyled my-0">
-          <li class="text-center w-100 p-2"><a href="#" class="text-white text-uppercase fw-bold">Sample1</a></li>
-          <li class="text-center w-100 p-2"><a href="#" class="text-white text-uppercase fw-bold">Sample1</a></li>
-          <li class="text-center w-100 p-2"><a href="#" class="text-white text-uppercase fw-bold">Sample1</a></li>
-          <li class="text-center w-100 p-2"><a href="#" class="text-white text-uppercase fw-bold">Sample1</a></li>
+          <?php
+          foreach ($categories->db as $key => $fields) :
+            if ($fields['list']) :
+              echo '<li class="text-center w-100 p-2"><a href="' . DOMAIN_CATEGORIES . $key . '" class="text-white text-uppercase fw-bold">' . $fields['name'] . '</a></li>';
+            endif;
+          endforeach;
+          ?>
         </ul>
       </nav>
     </div>
