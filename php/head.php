7a5758e09824f432fb7e0a423364715321750281
Jun Nogata <nogajun@gmail.com>
2022-07-31 23:43:53 +0900

    add google fonts config and prefetch

--- php/head.php
+++ php/head.php
@@ -3,9 +3,44 @@
 <meta name="generator" content="Bludit">
 <?php echo Theme::metaTags('title'); ?>
 <?php echo Theme::metaTags('description'); ?>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+<link rel="preconnect dns-prefetch" href="//www.googletagmanager.com">
+<link rel="preconnect dns-prefetch" href="//www.google-analytics.com">
+<link rel="preconnect dns-prefetch" href="//ajax.googleapis.com">
+<link rel="preconnect dns-prefetch" href="//cdnjs.cloudflare.com">
+<link rel="preconnect dns-prefetch" href="//pagead2.googlesyndication.com">
+<link rel="preconnect dns-prefetch" href="//googleads.g.doubleclick.net">
+<link rel="preconnect dns-prefetch" href="//tpc.googlesyndication.com">
+<link rel="preconnect dns-prefetch" href="//www.gstatic.com">
+<link rel="preconnect dns-prefetch" href="//cse.google.com">
+<link rel="preconnect dns-prefetch" href="//fonts.googleapis.com">
+<link rel="preconnect dns-prefetch" href="//fonts.gstatic.com">
+<link rel="preconnect dns-prefetch" href="//cdn.syndication.twimg.com">
+<link rel="preconnect dns-prefetch" href="//cdn.jsdelivr.net">
+<link rel="preconnect dns-prefetch" href="//images-fe.ssl-images-amazon.com">
+<link rel="preconnect dns-prefetch" href="//completion.amazon.com">
+<link rel="preconnect dns-prefetch" href="//m.media-amazon.com">
+<link rel='dns-prefetch' href='//cdnjs.cloudflare.com' />
+<link rel='dns-prefetch' href='//ajax.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<style type="text/css">
+  @font-face {
+    font-family: 'Noto Sans JP Regular';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 400;
+    src: local("Noto Sans CJK JP"),
+      url(<?php echo HTML_PATH_THEME . 'font/NotoSansCJKjp-Regular.woff2'; ?>) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Noto Sans JP Bold';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 700;
+    src: local("Noto Sans CJK JP"),
+      url(<?php echo HTML_PATH_THEME . 'font/NotoSansCJKjp-Bold.woff2'; ?>) format('woff2');
+  }
+</style>
 <link rel="stylesheet" href="<?php echo HTML_PATH_THEME_CSS . 'bootstrap.min.css'; ?>">
 <link rel="stylesheet" href="<?php echo HTML_PATH_THEME_CSS . 'bootstrap-icons.css'; ?>">
 <link rel="stylesheet" href="<?php echo HTML_PATH_THEME_CSS . 'style.css'; ?>">
